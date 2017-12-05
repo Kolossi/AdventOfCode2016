9ffb29e6b946f1a6c05a0d17efdeae872de0d8a5
Paul Sweeney <github@kolossi.co.uk>
2017-12-05 13:23:05 +0000

    day6

--- Runner/Day06.cs
+++ Runner/Day06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Runner
@@ -8,12 +9,40 @@ namespace Runner
     {
         public override string First(string input)
         {
-            throw new NotImplementedException();
+            var lines = GetLines(input);
+            var length = lines[0].Length;
+
+            return string.Join("",
+                Enumerable.Range(0, length)
+                    .Select(i => GetMostCommonChar(lines.Select(l => l[i]))));
+        }
+
+        public char GetMostCommonChar(IEnumerable<char> chars)
+        {
+            return chars.Distinct()
+                .Select(c => new { c = c, count = chars.Count(n => n == c) })
+                .OrderByDescending(x => x.count)
+                .Select(x => x.c)
+                .First();
+        }
+
+        public char GetLeastCommonChar(IEnumerable<char> chars)
+        {
+            return chars.Distinct()
+                .Select(c => new { c = c, count = chars.Count(n => n == c) })
+                .OrderBy(x => x.count)
+                .Select(x => x.c)
+                .First();
         }
 
         public override string Second(string input)
         {
-            throw new NotImplementedException();
+            var lines = GetLines(input);
+            var length = lines[0].Length;
+
+            return string.Join("",
+                Enumerable.Range(0, length)
+                    .Select(i => GetLeastCommonChar(lines.Select(l => l[i]))));
         }
     }
 }
