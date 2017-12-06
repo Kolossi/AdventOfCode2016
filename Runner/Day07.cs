f2188a31ed1d99079a77ed8738056a79fe91bc35
Paul Sweeney <psweeney@travelrepublic.co.uk>
2017-12-06 10:50:33 +0000

    day7

--- Runner/Day07.cs
+++ Runner/Day07.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -57,9 +58,43 @@ namespace Runner
             return (input[0] == input[3] && input[1] == input[2] && input[0] != input[1]);
         }
 
+        bool SupportsSSL(string input)
+        {
+            var matches = ipv7RE.Matches(input);
+
+            var hyperNets = matches.Select(m=>m.Groups[2].Value).Where(h => !string.IsNullOrWhiteSpace(h));
+
+            var otherNets = matches.Select(m => m.Groups[1].Value);
+
+            return (otherNets.SelectMany(n => GetABAs(n))).Any(a => hyperNets.Any(h => h.Contains(ToBAB(a))));
+        }
+
+        string[] GetABAs(string input)
+        {
+            var length = input.Length;
+            if (length < 3) return new string[]{};
+            return Enumerable.Range(0, length - 2).Select(i=>input.Substring(i, 3)).Where(x => IsABA(x)).ToArray();
+        }
+
+        bool IsABA(string input)
+        {
+            if (input.Length != 3) return false;
+            return (input[0] == input[2] && input[0] != input[1]);
+        }
+
+        string ToBAB(string aba)
+        {
+            return string.Join("", new Char[] {aba[1], aba[0], aba[1]});
+        }
+
         public override string Second(string input)
         {
-            throw new NotImplementedException();
+            return GetLines(input).Count(l => SupportsSSL(l)).ToString();
+        }
+
+        public override string SecondTest(string input)
+        {
+            return SupportsSSL(input) ? "1" : "0";
         }
     }
 }
