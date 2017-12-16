7f4e70e84b066c9e7370a913c9cff91f9c6902c2
Paul Sweeney <github@kolossi.co.uk>
2017-12-16 17:44:01 +0000

    day13

--- Runner/Day13.cs
+++ Runner/Day13.cs
@@ -18,35 +18,114 @@ namespace Runner
                 X = x;
                 Y = y;
             }
+
+            public override bool Equals(object obj)
+            {
+                var xy = obj as XY;
+                if (xy == null) return false;
+
+                return xy.X == X && xy.Y == Y;
+            }
+
+            public override int GetHashCode()
+            {
+                return 39 ^ X ^ Y;
+            }
+
+            public XY Clone()
+            {
+                return new XY(X, Y);
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0},{1}", X, Y);
+            }
         }
 
-        private XY[] Directions = new XY[] {
+        public class Walk
+        {
+            public List<XY> Previous { get; set; }
+            public XY XY { get; set; }
+            public int Dist { get; set; }
+            public Walk Clone()
+            {
+                var walk = new Walk()
+                {
+                    Previous = new List<XY>(Previous.ToArray()),
+                    XY = XY.Clone(),
+                    Dist = Dist+1,
+                };
+                Previous.Add(XY.Clone());
+                return walk;
+            }
+
+            public List<Walk> GetNextWalks()
+            {
+                var newWalks = new List<Walk>();
+                foreach (var dirXY in Directions)
+                {
+                    var walk = this.Clone();
+                    walk.XY.X += dirXY.X;
+                    walk.XY.Y += dirXY.Y;
+                    if (!walk.BeenHere()) newWalks.Add(walk);
+                }
+                return newWalks;
+            }
+
+            public bool BeenHere()
+            {
+                foreach (var prev in Previous)
+                {
+                    if (prev.Equals(XY)) return true;
+                }
+                return false;
+            }
+        }
+
+        private static XY[] Directions = new XY[] {
             new XY(0, 1),
             new XY(1, 0),
             new XY(0, -1),
-            new XY(-1, 0)
+            new XY(-1, 0),
         };
 
-        public override string First(string input)
+        public int WalkIt(XY start, XY destination, int favourite, int walkLimit=int.MaxValue)
         {
-            Map = new Dictionary<XY, int>();
-            var favourite = int.Parse(input);
-            var sb = new StringBuilder();
-            for (int y = 0; y < 6; y++)
+            int minDistance = walkLimit;
+            var firstWalk = new Walk()
             {
-                for (int x = 0; x < 9; x++)
+                Dist = 0,
+                Previous = new List<XY>(),
+                XY = start
+            };
+
+            var toWalk = new Queue<Walk>();
+            toWalk.Enqueue(firstWalk);
+
+            while (toWalk.Any())
+            {
+                var walk = toWalk.Dequeue();
+                if (IsWall(walk.XY, favourite)) continue;
+
+                Map[walk.XY] = Math.Min(Map[walk.XY], walk.Dist);
+                if (minDistance < int.MaxValue && walk.Dist > minDistance) continue;
+
+                if (walk.XY.Equals(destination))
                 {
-                    sb.Append(IsWall(new XY(x, y), favourite) ? "#" : ".");
+                    Console.WriteLine(string.Format("Made it in {0}", walk.Dist));
+                    minDistance = Math.Min(minDistance, walk.Dist);
+                    continue;
                 }
-                sb.AppendLine();
+
+                var newWalks = walk.GetNextWalks();
+                foreach (var newWalk in newWalks)
+                {
+                    toWalk.Enqueue(newWalk);
+                }
             }
 
-            return sb.ToString();
-        }
-
-        public override string Second(string input)
-        {
-            throw new NotImplementedException();
+            return minDistance;
         }
 
         public bool IsWall(XY xy, int favourite)
@@ -54,7 +133,7 @@ namespace Runner
             int mapValue;
             if (Map.TryGetValue(xy, out mapValue))
             {
-                if (mapValue < 0) return true;
+                return (mapValue < 0);
             }
 
             if (xy.X < 0 || xy.Y < 0) return true;
@@ -63,8 +142,27 @@ namespace Runner
 
             UInt64 result = x * x + 3UL * x + 2UL * x * y + y + y * y + (UInt64)favourite;
             var isWall =  (Convert.ToString((int)result, 2).Count(c => c == '1') % 2) == 1;
-            Map[xy] = isWall ? -1 : 0;
+            Map[xy] = isWall ? -1 : int.MaxValue;
             return isWall;
+        }
+
+        public override string First(string input)
+        {
+            Map = new Dictionary<XY, int>();
+            var parts = input.Split(",");
+            var destination = new XY(int.Parse(parts[0]), int.Parse(parts[1]));
+            var favourite = int.Parse(parts[2]);
+            return WalkIt(new XY(1, 1), destination, favourite).ToString();
+        }
+
+        public override string Second(string input)
+        {
+            Map = new Dictionary<XY, int>();
+            var parts = input.Split(",");
+            var destination = new XY(int.Parse(parts[0]), int.Parse(parts[1]));
+            var favourite = int.Parse(parts[2]);
+            WalkIt(new XY(1, 1), destination, favourite, 50).ToString();
+            return Map.Values.Count(v => v >= 0 && v <=50).ToString();
         }
     }
 }
