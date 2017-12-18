f8b88934587018f26d594bb402c037e76ac10b2c
paul <git@kolossi.co.uk>
2017-12-18 19:47:08 +0000

    day15

--- Runner/Day15.cs
+++ Runner/Day15.cs
@@ -1,19 +1,65 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Runner
 {
     class Day15 : Day
     {
+        class Disc
+        {
+            public int Positions { get; set; }
+            public int StartPos { get; set; }
+        }
+
+        private static int FindDelay(Disc[] discs)
+        {
+            int delay = 0;
+            do
+            {
+                bool caught = false;
+                for (int time = 1; time <= discs.Length; time++)
+                {
+                    var disc = discs[time - 1];
+                    var position = (disc.StartPos + time + delay) % disc.Positions;
+                    if (position != 0)
+                    {
+                        caught = true;
+                        break;
+                    }
+                }
+                if (!caught) return delay;
+                delay++;
+            } while (true);
+        }
+
+        private Disc[] GetDiscs(string input)
+        {
+            var discs = new List<Disc>();
+            foreach (var line in GetLines(input))
+            {
+                var parts = GetParts(line);
+                discs.Add(new Disc()
+                {
+                    Positions = int.Parse(parts[3]),
+                    StartPos = int.Parse(parts[11].Replace(".", ""))
+                });
+            }
+            return discs.ToArray();
+        }
+
         public override string First(string input)
         {
-            throw new NotImplementedException();
+            var discs = GetDiscs(input);
+            return FindDelay(discs).ToString();
         }
 
         public override string Second(string input)
         {
-            throw new NotImplementedException();
+            var discs = GetDiscs(input);
+            discs = (new List<Disc>(discs)).Union(new Disc[] { new Disc() { Positions = 11, StartPos = 0 } }).ToArray();
+            return FindDelay(discs).ToString();
         }
     }
 }
