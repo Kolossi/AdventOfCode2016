ef420956df4b99f123703c8712ef08922702c767
Paul Sweeney <github@kolossi.co.uk>
2017-12-26 19:34:47 +0000

    day19

--- Runner/Day19.cs
+++ Runner/Day19.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,121 +8,76 @@ namespace Runner
 {
     class Day19 : Day
     {
-        //public class Elf
-        //{
-        //    public int Seat { get; set; }
-        //    public int Presents { get; set; }
-        //}
-
-        //public override string First(string input)
-        //{
-        //    int elfCount = int.Parse(input);
-        //    var elves = Enumerable.Range(1, elfCount).Select(i => new Elf() { Seat = i, Presents = 1 }).ToArray();
-        //    do
-        //    {
-        //        var i = 0;
-        //        while (i < elfCount - 1)
-        //        {
-        //            Steal(elves, i, i + 1);
-        //            i += 2;
-        //        }
-        //        if (i == elfCount - 1)
-        //        {
-        //            Steal(elves, i, 0);
-        //        }
-        //        elves = elves.Where(e => e.Presents > 0).ToArray();
-        //        elfCount = elves.Length;
-
-        //    }
-        //    while (elves.Count() > 1);
-
-        //    return elves.First().Seat.ToString();
-        //}
-
-        //private static void Steal(Elf[] elves, int taker, int from)
-        //{
-        //    var elf = elves[taker];
-        //    var nextElf = elves[from];
-        //    elves[taker].Presents += elves[from].Presents;
-        //    elves[from].Presents = 0;
-        //}
-
         public class Elf
         {
             public int Seat { get; set; }
             public bool Presents { get; set; }
-        }
-
-        private static void GameElves(int elfCount, Dictionary<int, Elf> elves, Func<Dictionary<int,Elf>,int,int,int> stealFromFn)
-        {
-            int maxElf = elfCount;
-            do
-            {
-                var current = 0;
-                while (current < maxElf && elves.Keys.Count() > 1)
-                {
-                    current = GetNextElf(elves, current + 1, maxElf);
-                    var stealFrom = stealFromFn(elves, current, maxElf);
-                    Steal(elves, current, stealFrom);
-                }
-                maxElf = elves.Keys.Max();
-            }
-            while (elves.Keys.Count() > 1);
-        }
-
-        private static int GetNextElf(Dictionary<int, Elf> elves, int i, int maxElf)
-        {
-            if (!elves.Any()) throw new InvalidOperationException();
-            while (!elves.ContainsKey(i))
-            {
-                i++;
-                if (i > maxElf) i = 1;
-            }
-            return i;
-        }
-
-        private static void Steal(Dictionary<int, Elf> elves, int taker, int from)
-        {
-            var elf = elves[taker];
-            var nextElf = elves[from];
-            elves.Remove(from);
-        }
-
-        //private static int GetOppositeElf(Dictionary<int, Elf> elves, int current, int maxElf)
-        //{
-        //    int[] seatIndexes = elves.Keys.OrderBy(seat => seat).ToArray();
-        //    var currentIndex = Array.IndexOf(seatIndexes, current);
-        //    var oppositeIndex = (currentIndex + (seatIndexes.Length / 2)) % seatIndexes.Length;
-        //    return seatIndexes[oppositeIndex];
-        //}
-
-        private static int GetOppositeElf(Dictionary<int, Elf> elves, int current, int maxElf)
-        {
-            var skipForward = elves.Count() / 2;
-            var next = current;
-            for (int i = 0; i < skipForward; i++)
-            {
-                next = GetNextElf(elves, next + 1, maxElf);
-            }
-            return next;
+            public Elf Next { get; set; }
+            public Elf Prev { get; set; }
         }
 
         public override string First(string input)
         {
             int elfCount = int.Parse(input);
-            var elves = Enumerable.Range(1, elfCount).Select(i => new Elf() { Seat = i, Presents = true }).ToDictionary(e => e.Seat);
-            GameElves(elfCount, elves, (e, current, maxElf) => GetNextElf(e, current + 1, maxElf));
+            var elves = Enumerable.Range(1, elfCount).Select(i => new Elf() { Seat = i, Presents = true }).ToArray();
+            do
+            {
+                var i = 0;
+                while (i < elfCount - 1)
+                {
+                    Steal(elves, i, i + 1);
+                    i += 2;
+                }
+                if (i == elfCount - 1)
+                {
+                    Steal(elves, i, 0);
+                }
+                elves = elves.Where(e => e.Presents).ToArray();
+                elfCount = elves.Length;
 
-            return elves.Values.First().Seat.ToString();
+            }
+            while (elves.Count() > 1);
+
+            return elves.First().Seat.ToString();
+        }
+
+        private static void Steal(Elf[] elves, int taker, int from)
+        {
+            elves[from].Presents = false;
+        }
+
+        private static void Delete(Elf elf)
+        {
+            elf.Prev.Next = elf.Next;
+            elf.Next.Prev = elf.Prev;
         }
 
         public override string Second(string input)
         {
             int elfCount = int.Parse(input);
-            var elves = Enumerable.Range(1, elfCount).Select(i => new Elf() { Seat = i, Presents = true }).ToDictionary(e => e.Seat);
-            GameElves(elfCount, elves, (e, current, maxElf) => GetOppositeElf(e, current, maxElf));
+            var elves = Enumerable.Range(1, elfCount).Select(i => new Elf() { Seat = i, Presents = true }).ToArray();
+            for (int i = 0; i < elfCount; i++)
+            {
+                elves[i].Next = elves[(i + 1) % elfCount];
+                elves[i].Prev = elves[(elfCount + i - 1) % elfCount];
+            }
 
-            return elves.Values.First().Seat.ToString();
+            var elf = elves[0];
+            var firstElf = elf;
+            var stealFrom = elves[elfCount / 2];
+
+            do
+            {
+                Delete(stealFrom);
+                if (stealFrom == firstElf) firstElf = firstElf.Next;
+                elfCount--;
+                stealFrom = stealFrom.Next;
+                if (elfCount % 2 == 0) stealFrom = stealFrom.Next;
+                elf = elf.Next;
+                
+            }
+            while (elfCount > 1);
+            return elf.Seat.ToString();
         }
     }
 }
